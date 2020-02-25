b60bbedc7ce70280fcc9e45ee3396590e69e82d7
Ashley Thomas <avthomas@usc.edu>
2020-02-25 15:27:51 -0800

    Added red hang up button

--- Assets/Scripts/PhoneIcon.cs
+++ Assets/Scripts/PhoneIcon.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhoneIcon : MonoBehaviour
 {
     public GameObject phone;
     public GameObject blur;
+    public Sprite redPhone;
+    public Sprite greenPhone;
+    public Button phoneButton;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -21,14 +25,18 @@ public class PhoneIcon : MonoBehaviour
 
     public void IconClicked()
     {
+        //Phone is already open, want to close it
         if (phone.activeInHierarchy)
         {
+            phoneButton.image.sprite = greenPhone;
             blur.SetActive(false);
             phone.SetActive(false);
         }
 
+        //Open phone
         else
         {
+            phoneButton.image.sprite = redPhone;
             blur.SetActive(true);
             phone.SetActive(true);
         }
