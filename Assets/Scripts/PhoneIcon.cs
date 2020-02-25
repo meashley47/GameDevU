c725cfc9a3599ef30f31c93ee92768865d453ab2
Ashley Thomas <avthomas@usc.edu>
2020-02-25 15:02:23 -0800

    Added phone button and blur effect when phone opens

---
+++ Assets/Scripts/PhoneIcon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneIcon : MonoBehaviour
+{
+    public GameObject phone;
+    public GameObject blur;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void IconClicked()
+    {
+        if (phone.activeInHierarchy)
+        {
+            blur.SetActive(false);
+            phone.SetActive(false);
+        }
+
+        else
+        {
+            blur.SetActive(true);
+            phone.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ProcessKeyPress();
+    }
+
+    void ProcessKeyPress()
+    {
+        transform.Translate(Input.GetAxisRaw("Horizontal") * Vector2.right * movementSpeed * Time.deltaTime);
+        transform.Translate(Input.GetAxisRaw("Vertical") * Vector2.up * movementSpeed * Time.deltaTime);
+    }
+}
