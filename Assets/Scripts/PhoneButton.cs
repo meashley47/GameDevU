03b03801c4b3dc36bf0e2b19029b48a9e35d3d4c
Ashley Thomas <avthomas@usc.edu>
2020-02-26 00:40:22 -0800

    Added animations to phone button ring

---
+++ Assets/Scripts/PhoneButton.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneButton : MonoBehaviour
+{
+    public GameObject phone;
+    public GameObject blur;
+    public Sprite redPhone;
+    public Sprite greenPhone;
+    Button button;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = this.gameObject.GetComponent<Button>();
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void IconClicked()
+    {
+        //Closing the phone UI
+        if (phone.activeInHierarchy)
+        {
+            button.image.sprite = greenPhone;
+            blur.SetActive(false);
+            phone.SetActive(false);
+
+            //Temporary, just to remind us the phone can ring
+            PhoneIsRinging(true);
+        }
+
+        //Opening the phone UI
+        else
+        {
+            button.image.sprite = redPhone;
+            blur.SetActive(true);
+            phone.SetActive(true);
+            PhoneIsRinging(false);
+        }
+    }
+
+    public void PhoneIsRinging(bool ringing)
+    {
+        animator.SetBool("ringing", ringing);
+    }
+}
