7fc640c2360515d43285d838a55e1a34cdca64cb
Ashley Thomas <avthomas@usc.edu>
2020-03-07 17:40:37 -0800

    Canvas and all its buttons will persist through different scenes

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -5,10 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour
 {
+    public Canvas canvas;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        DontDestroyOnLoad(this);
+        DontDestroyOnLoad(canvas);
     }
 
     // Update is called once per frame
